830cbd62a801f94662d3b852d8c8f92d19c59000
IvanBushtin <44139811+IvanBushtin@users.noreply.github.com>
2019-03-06 14:24:27 +0200

    add header, nav menu and top-section, bg with linear-gradient.

--- build/js/script.js
+++ build/js/script.js
@@ -1,4 +1,14 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var hamburger = document.getElementById('openMenu');
+    var navList =  document.getElementsByClassName('d-none')[0];
 
+    hamburger.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        navList.classList.toggle('d-flex');
+        hamburger.classList.toggle('rotate');
+    });
+});
 
 
 
