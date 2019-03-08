b275dd68247770273363d589119f364722d7feab
IvanBushtin <ivanbushtin77@gmail.com>
2019-03-08 18:44:16 +0200

    update

--- build/js/script.js
+++ build/js/script.js
@@ -10,5 +10,17 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+var elem = document.querySelector('.grid');
+var msnry = new Masonry( elem, {
+    // options
+    itemSelector: '.grid-item',
+    columnWidth: 200
+});
+
+// element argument can be a selector string
+//   for an individual element
+var msnry = new Masonry( '.grid', {
+    // options
+});
 
 
